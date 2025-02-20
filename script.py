257d8baa4e2d8be60140b8a20f1e6b6a34ab848f
MarounnGhayaa <marounghaya@icloud.com>
2025-02-20 18:59:01 +0200

    Adding the salary raised to the second power

--- script.py
+++ script.py
@@ -21,16 +21,15 @@ while action != "no":
     }
     salary_list.append(calculations)
     action = input("Do you want to proceed with another month (yes/no): ")
-print(salary_list)
 print("=" * 20)
 print("Salaries summary!")
 print("=" * 20)
 for salary in salary_list:
     print(f"Month: {salary['month']}")
-    print(f"Salary: {salary['salary']}$")
-    print(f"Savings: {salary['savings']}% --> {salary['allocated_savings']}$")
-    print(f"Rent: {salary['rent']}% --> {salary['allocated_rent']}$")
-    print(f"Electricity: {salary['electricity']}% --> {salary['allocated_electricity']}$")
+    print(f"Salary: {salary['salary']}$ - [salary^2 = {salary['salary'] ** 2}]")
+    print(f"Savings: {salary['savings']}%       -->       {salary['allocated_savings']}$")
+    print(f"Rent: {salary['rent']}%          -->       {salary['allocated_rent']}$")
+    print(f"Electricity: {salary['electricity']}%   -->       {salary['allocated_electricity']}$")
     print("=" * 40)
 """ 
 total_amount = allocated_savings + allocated_rent + allocated_electricity
@@ -38,6 +37,5 @@ print(f"Your total amount on allocations is: {total_amount}$.")
 remainder = salary - total_amount
 print(f"Your remainder is: {remainder}$.")
 print(f"Your estimated yearly rent is {allocated_rent * 12}$, and your estimated yearly electricity costs are {allocated_electricity * 12}")
-print(salary ** 2)
 random_money = 50.0
 print(f"If each month you had {random_money}$ for a year, {(random_money * 12) / (allocated_savings * 12)}$ will be left after diving by total amount allocated to savings.") """
