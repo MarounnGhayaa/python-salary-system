354b75300a1977974241f5f71df71bd14613ca03
MarounnGhayaa <marounghaya@icloud.com>
2025-02-20 18:09:31 +0200

    Making the program work for several months

--- script.py
+++ script.py
@@ -1,11 +1,28 @@
-salary = float(input("Enter your salary: "))
-month = input("Enter the month you're storing the salary for: ")
-savings = int(input("Enter your savings percentage: "))
-rent = int(input("Enter your rent percentage: "))
-electricity = int(input("Enter your electricity percentage: "))
-allocated_savings = salary * savings / 100
-allocated_rent =  salary * rent / 100
-allocated_electricity = salary * electricity / 100
+action = "yes"
+list = []
+while action != "no":
+    salary = float(input("Enter your salary: "))
+    month = input("Enter the month you're storing the salary for: ")
+    savings = float(input("Enter your savings percentage: "))
+    rent = float(input("Enter your rent percentage: "))
+    electricity = int(input("Enter your electricity percentage: "))
+    allocated_savings = salary * savings / 100
+    allocated_rent =  salary * rent / 100
+    allocated_electricity = salary * electricity / 100
+    calculations = {
+        "salary": salary,
+        "month": month,
+        "savings": savings,
+        "rent": rent,
+        "electricity": electricity,
+        "allocated_savings": allocated_savings,
+        "allocated_rent": allocated_rent,
+        "allocated_electricity": allocated_electricity,
+    }
+    list.append(calculations)
+    action = input("Do you want to proceed with another month (yes/no): ")
+print(list)
+""" 
 print(f"Your salary {salary}$") 
 print(f"Your savings percentage {savings}%, your allocated savings amount will be {allocated_savings}$")
 print(f"Your rent percentage {rent}%, your allocated rent amount will be {allocated_rent}$")
@@ -17,4 +34,4 @@ print(f"Your remainder is: {remainder}$.")
 print(f"Your estimated yearly rent is {allocated_rent * 12}$, and your estimated yearly electricity costs are {allocated_electricity * 12}")
 print(salary ** 2)
 random_money = 50.0
-print(f"If each month you had {random_money}$ for a year, {(random_money * 12) / (allocated_savings * 12)}$ will be left after diving by total amount allocated to savings.")
+print(f"If each month you had {random_money}$ for a year, {(random_money * 12) / (allocated_savings * 12)}$ will be left after diving by total amount allocated to savings.") """
